e61e11b7eaf70b6beedac00ce6ee500e6c425033
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-07-05 11:40:29 +0700

    Add info log

--- Authorize.Fody/ModuleWeaver.cs
+++ Authorize.Fody/ModuleWeaver.cs
@@ -1,4 +1,5 @@
 ﻿using Mono.Cecil;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,25 +12,47 @@ namespace Authorize.Fody
         public string SolutionDirectoryPath { get; set; }
         public string ProjectDirectoryPath { get; set; }
 
+        // Will log an MessageImportance.High message to MSBuild. OPTIONAL
+        public Action<string> LogInfo { get; set; }
 
-        private string GetDll(string root)
+        private Tuple<bool, string> GetDll(string root)
         {
             var packageDir = Path.Combine(root, "packages");
             var packageInfo = new DirectoryInfo(packageDir);
-            var webApi = packageInfo.GetDirectories("Microsoft.AspNet.WebApi.Core.*").LastOrDefault().FullName;
-            return Path.Combine(webApi, @"lib\net45", "System.Web.Http.dll");
+
+            if (packageInfo.Exists)
+            {
+                var webApi = packageInfo.GetDirectories("Microsoft.AspNet.WebApi.Core.*").LastOrDefault().FullName;
+                var path = Path.Combine(webApi, @"lib\net45", "System.Web.Http.dll");
+                return new Tuple<bool, string>(true, path);
+            }
+
+            return new Tuple<bool, string>(false, "");
         }
 
         private string DllPath()
         {
-            try
+            LogInfo($" >> Solution Path | {SolutionDirectoryPath}");
+            LogInfo($" >> Project Path | {ProjectDirectoryPath}");
+            LogInfo($" >> Current Path | ${new DirectoryInfo("./").FullName}");
+
+            var solution = GetDll(SolutionDirectoryPath);
+            if (solution.Item1) return solution.Item2;
+
+            var project = GetDll(ProjectDirectoryPath);
+            if (project.Item1) return project.Item2;
+
+            var dir = new DirectoryInfo("./");
+            while (dir.Parent != null)
             {
-                return GetDll(SolutionDirectoryPath);
+                LogInfo($" >> Current Dir | {dir.FullName}");
+                var current = GetDll(dir.FullName);
+                if (current.Item1) return current.Item2;
+
+                dir = dir.Parent;
             }
-            catch
-            {
-                return GetDll(ProjectDirectoryPath);
-            }
+
+            return "";
         }
 
         public void Execute()
@@ -57,7 +80,6 @@ namespace Authorize.Fody
 
             var asm = AssemblyDefinition.ReadAssembly(dllPath);
             var attr = asm.MainModule.GetType(attributeName);
-            //var attr = module.GetType(attributeName);
 
             var ctor = attr.Methods.First(x => x.IsConstructor);
             var ctorReference = module.ImportReference(ctor);
--- build.cake
+++ build.cake
@@ -1,6 +1,19 @@
 var target = Argument("target", "Default");
 var npi = EnvironmentVariable("npi");
 
+#tool "nuget:?package=NUnit.ConsoleRunner"
+#tool "nuget:?package=xunit.runner.console&version=2.1.0"
+// #tool "nuget:?package=xunit.runners&version=1.9.2"
+
+Task("test")
+    .Does(() => {
+        //XBuild("Authorize.Fody.sln");
+        var settings = new XUnit2Settings {
+            ToolPath = @"tools\xunit.runner.console\tools\xunit.console.x86.exe"
+        };
+        XUnit2("./Authorize.Fody.Tests/bin/Debug/Authorize.Fody.Tests.dll", settings);
+});
+
 Task("push")
     .Description("Push nuget")
     .Does(() => {
